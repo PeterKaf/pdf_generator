7443e23dd9c253d8fcd308c7c2d2ec4fffaea03c
PiotrKafel <marapablo891@gmail.com>
2023-07-17 16:36:47 +0200

    Added column titles, fixed formatting

--- main.py
+++ main.py
@@ -22,17 +22,28 @@ for filepath in invoice_filepaths:
 
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=8, txt=f'Customer: {df.iloc[0,1]}', ln=1)
+    pdf.cell(w=50, h=8, txt="", ln=1)
+
+    columns = list(df.columns)
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.cell(w=50, h=8, txt=columns[0], border=1)
+    pdf.cell(w=30, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=40, h=8, txt=columns[3], border=1, ln=1)
 
     for index, row in df.iterrows():
         if index < len(df) - 1:
             pdf.set_font(family="Times", size=10)
             pdf.cell(w=50, h=8, txt=str(row['Product ID']), border=1)
             pdf.cell(w=30, h=8, txt=str(row['Quantity']), border=1)
-            pdf.cell(w=30, h=8, txt=str(row['Unit Price']), border=1)
-            pdf.cell(w=30, h=8, txt=str(row['Total Amount']), border=1, ln=1)
+            pdf.cell(w=40, h=8, txt=str(row['Unit Price']), border=1)
+            pdf.cell(w=40, h=8, txt=str(row['Total Amount']), border=1, ln=1)
         elif index == len(df) - 1:
-            pdf.set_font(family="Times", style="B", size=16)
-            pdf.cell(w=50, h=8, txt=f'Total Price: {df.iloc[index, 5]}', ln=1)
+            pdf.set_font(family="Times", style="B", size=10)
+            pdf.cell(w=50, h=8, txt="", border=1)
+            pdf.cell(w=30, h=8, txt="", border=1)
+            pdf.cell(w=40, h=8, txt="Total Price:", border=1)
 
+            pdf.cell(w=40, h=8, txt=df.iloc[index, 5], ln=1, border=1)
 
     pdf.output(f'output/{filename}.pdf')
