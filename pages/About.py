e4a3623572dcd43d841a0ca01890b06724347f9e
Piotr Kafel <peterkaf132@gmail.com>
2023-07-19 13:15:41 +0200

    Finished About.py, added template image

--- pages/About.py
+++ pages/About.py
@@ -6,10 +6,17 @@ message = """The Invoice to PDF Converter is a web application that allows you t
 into PDF format. With this app, you can easily upload your invoice files, convert them to PDF,
 and download the generated PDF files."""
 
+image = Image.open("Invoice_template.png")
+
+
 st.write(message)
 st.header("How to use the app")
-st.write("1. Prepare your invoice files in XLSX format, according to the template provided.")
-st.write("2. Upload your invoice files using the file uploader.")
-st.write("3. Click the 'Convert to PDF' button to start the conversion process.")
-st.write("4. After the conversion process is complete, you can download the generated PDF files.")
+st.write("1. Name invoice files so that invoice number is prefaced with '-' e.q: 'Invoice-<invoice number>.xlsx'.")
+st.write("2. Prepare your invoice files in XLSX format, according to the template provided.")
+st.image(image)
+st.write("The first row of the XLSX file should contain the exact column titles."
+         " date and customer name should be entered as shown in red. Total price needs to be calculated at the bottom.")
+st.write("3. Upload your invoice files using the file uploader (click 'Browse files' or use drag and drop.)")
+st.write("4. Click the 'Convert to PDF' button to start the conversion process.")
+st.write("5. After the conversion process is complete, you can download the generated PDF files.")
 
